4674d6ec6dbdf303d1d92315b23e016fe67d6782
psable <52033359+psable@users.noreply.github.com>
2021-12-07 11:35:34 +0100

    Update index.php

--- php/index.php
+++ php/index.php
@@ -2,4 +2,22 @@
  echo "<h1> Hello from DOCKER PHP formation 2021</h1>";
  echo "<h1>Hello World from PHP7-FPM</h1>";
  echo 'Version PHP courante : ' . phpversion();
+ $bdd = getenv ("BDD_HOST");
+ $user = getenv ("BDD_USER");
+ $pass = getenv ("BDD_PASS");
+ echo '<pre>';
+ try{
+   $conn = new \PDO("mysql:host=$bdd", "$user", "$pass");
+   $sth = $conn->prepare('SHOW DATABASES');
+   $sth->execute();
+   $checks = $sth->fetchAll(PDO::FETCH_ASSOC);
+   foreach ($checks as $check) {
+ 
+   print_r($check);
+   }
+ }
+ catch(\Exception $e){
+     print_r($e);
+ }
+ echo '</pre>';
 ?>
